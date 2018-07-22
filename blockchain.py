01e04e41d13a268de1175cba99626f95001ef4df
Shashi Khanal <khanalshashi@gmail.com>
2018-07-22 15:47:10 +0545

    multithreading enabled

--- blockchain.py
+++ blockchain.py
@@ -418,4 +418,4 @@ def getBalance():
 
 if __name__ == '__main__':
     port = int(sys.argv[1])
-    app.run(host = '0.0.0.0', port = port)
+    app.run(host = '0.0.0.0', port = port, threaded=True)
